c74ec436426054538fe84aeb013df4020871802a
Jiaqi Meng <jiaqimeng@berkeley.edu>
2024-06-10 22:43:15 -0700

    fix quotes

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import streamlit as st
 import fitz
 import json
 
-def display_pdf_image(file_path, page_number):
-    document = fitz.open(file_path)
+
+def display_pdf_image(document, page_number):
     page = document.load_page(page_number)
     pix = page.get_pixmap()
     img = pix.tobytes("ppm")
-    st.image(img, caption=f"Page {page_number} of {num_pages}", use_column_width=True)
+    st.image(
+        img, caption=f"Page {page_number + 1} of {num_pages}", use_column_width=True
+    )
     return img
+
 
 def display_markdown(data, page_number):
     st.info("Markdown below")
     for page in data:
         if page["page_number"] == page_number:
             st.markdown(page["markdown"])
+
 
 def display_tables(data, page_number):
     optional = st.empty()
@@ -27,20 +31,21 @@ def display_tables(data, page_number):
                 if len(page["charts"]) > 0:
                     for chart in page["charts"]:
                         has_data = True
-                        st.markdown(f"chart name: {chart["name"]}")
-                        st.markdown(f"chart description: {chart["description"]}")
+                        st.markdown(f"chart name: {chart['name']}")
+                        st.markdown(f"chart description: {chart['description']}")
                         st.table(chart["data"])
                 if len(page["tables"]) > 0:
                     for chart in page["tables"]:
                         has_data = True
-                        st.markdown(f"table name: {chart["name"]}")
-                        st.markdown(f"table description: {chart["description"]}")
+                        st.markdown(f"table name: {chart['name']}")
+                        st.markdown(f"table description: {chart['description']}")
                         st.table(chart["data"])
     if not has_data:
         optional.empty()
+
+
 with st.sidebar:
-    st.header("Roe AI PDF Parser Visualizer",
-    )
+    st.header("Roe AI PDF Parser Visualizer", divider="rainbow")
     uploaded_pdf = st.file_uploader("Choose a PDF file", type="pdf")
 
     json_input = st.text_area("Paste JSON content here")
@@ -54,14 +59,11 @@ with st.sidebar:
 
 if uploaded_pdf is not None and json_data is not None:
     with st.sidebar:
-        with open("temp.pdf", "wb") as f:
-            f.write(uploaded_pdf.getvalue())
-
-        doc = fitz.open("temp.pdf")
+        doc = fitz.open("pdf", uploaded_pdf.getvalue())
         num_pages = doc.page_count
-        page_number = st.session_state.get("page_number", 1) 
-        
-        col1, col2 = st.columns([1,1])
+        page_number = st.session_state.get("page_number", 1)
+
+        col1, col2 = st.columns([1, 1])
         with col1:
             if st.button("Previous Page"):
                 if page_number > 1:
@@ -72,7 +74,7 @@ if uploaded_pdf is not None and json_data is not None:
                 if page_number < num_pages:
                     page_number += 1
                     st.session_state["page_number"] = page_number
-        display_pdf_image("temp.pdf", page_number - 1)
+        display_pdf_image(doc, page_number - 1)
 
     display_markdown(json_data, page_number)
     display_tables(json_data, page_number)
